c53cb5210e3f17d45b3b14f3153002d5f7978f39
George Gkasdrogkas <georgegkas@gmail.com>
2019-07-03 14:13:50 +0300

    feat: create the public API of this package
    This commit defines the public interface of this package, the
    exported functions, enums and traits, without implementing anything.

---
+++ src/error.rs
@@ -0,0 +1,43 @@
+//! Error types
+
+// TODO: Add documentation examples to `description`, `code` and `new` functions.
+
+/// A list of all error variants used by this module.
+pub enum ErrorType {}
+
+/// Used to represent the various error variants in this module.
+impl ErrorType {
+  /// Return the error description.
+  pub fn description(&self) -> &str {
+    unimplemented!();
+  }
+
+  /// Return the error code.
+  pub fn code(&self) -> &str {
+    unimplemented!();
+  }
+}
+
+/// Error type of validation functions.
+pub struct Error {
+  /// The ErrorType variant that describes the current error.
+  pub err: ErrorType,
+
+  /// The zero-based index before which the error ends.
+  /// The character at this index will not be included.
+  pub index_end: usize,
+
+  /// The zero-based index at which the error begins.
+  pub index_start: usize,
+}
+
+impl Error {
+  /// Construct a new error in the specified range.
+  pub fn new(err: ErrorType, index_start: usize, index_end: usize) -> Error {
+    Error {
+      err,
+      index_start,
+      index_end,
+    }
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod error;
+
+use error::Error;
+
+// Public exports
+pub use error::ErrorType;
+
+/// Validate a JSON document based on RFC 8259 latest standard.
+pub fn validate(_code: &str) -> Vec<Error> {
+  unimplemented!();
 }
