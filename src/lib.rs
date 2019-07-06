afbe98bd7acbe5f64be9cc6a6418d31c69870c23
George Gkasdrogkas <georgegkas@gmail.com>
2019-07-06 12:37:09 +0300

    feat: implement Tokens struct
    Tokens structure contains info about the whole parsing and validating process.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 mod error;
+mod tokens;
 
 use error::Error;
+use tokens::Tokens;
 
 // Public exports
 pub use error::ErrorType;
 
 /// Validate a JSON document based on RFC 8259 latest standard.
-pub fn validate(_code: &str) -> Vec<Error> {
-  unimplemented!();
+pub fn validate(code: &str) -> Vec<Error> {
+  let tokens = Tokens::new(code.chars().enumerate().peekable());
+
+  tokens.errors
 }
