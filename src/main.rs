36a6b0692a8962faf7c604fa13cf199479a01ab8
George Gkasdrogkas <georgegkas@gmail.com>
2019-07-23 13:50:14 +0300

    refactor: implement a stable CLI

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,32 @@
 extern crate jsonprima;
 
-use std::env;
-use std::fs;
-use std::path::Path;
-
-// TODO: Implement a proper CLI tool using a package like `clap`.
+use clap::{App, Arg};
 
 fn main() {
-  let args: Vec<_> = env::args().skip(1).collect();
+  let matches = App::new("JSONPrima")
+    .author("George Gkasdrogkas <georgegkas@gmail.com>")
+    .about("RFC 8259 compliant JSON validator in Rust.")
+    .arg(
+      Arg::with_name("input")
+        .short("i")
+        .long("input")
+        .value_name("INPUT")
+        .help("Load JSON document from CLI")
+        .takes_value(true),
+    )
+    .get_matches();
 
-  match args.first() {
-    Some(option) => match &option[..] {
-      "-f" => {
-        let p = Path::new(args.get(1).unwrap());
+  if !matches.is_present("input") {
+    println!("You should pass one valid CLI argument.");
+    std::process::exit(-1);
+  }
 
-        if !p.is_file() {
-          print!("Not a valid text file.");
-        }
+  let errors = jsonprima::validate(matches.value_of("input").unwrap_or(""));
 
-        let text = fs::read_to_string(p).expect("Something went wrong reading the file");
-
-        let errors = jsonprima::validate(&text);
-
-        if errors.is_empty() {
-          std::process::exit(0);
-        } else {
-          std::process::exit(1);
-        }
-      }
-
-      "-t" => {
-        let text = args.get(1).unwrap();
-
-        let errors = jsonprima::validate(&text);
-
-        if errors.is_empty() {
-          std::process::exit(0);
-        } else {
-          std::process::exit(1);
-        }
-      }
-
-      _ => println!("Unrecognized parameter."),
-    },
-
-    None => (println!("Specify a mode: '-f' for file, '-t' for text")),
+  if errors.is_empty() {
+    std::process::exit(0);
+  } else {
+    println!("{:#?}", errors);
+    std::process::exit(1);
   }
 }
